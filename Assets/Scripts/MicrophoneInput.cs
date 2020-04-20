5144126a4c6832444c37356b4d894857f4c7f696
Hessel Bonenkamp <hessel@mediamonks.com>
2020-04-20 17:26:26 +0200

    Refactored microphone input to be more responsive

---
+++ Assets/Scripts/MicrophoneInput.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Android;
+
+public sealed class MicrophoneInput : MonoBehaviour
+{
+	[SerializeField] private AudioSource _audioSource;
+	[SerializeField] private int _samplesSize = 128;
+
+	[SerializeField] private TMP_Text _peakText;
+	[SerializeField] private TMP_Text _maxText;
+	[SerializeField] private TMP_Text _positionText;
+
+	public float Value { get; private set; }
+	public string Device { get; private set; }
+	public float Peak { get; private set; }
+
+	private float[] _sampleValues;
+
+	private void Awake()
+	{
+		_sampleValues = new float[_samplesSize];
+	}
+
+	private IEnumerator Start()
+	{
+		// TODO proper permissions
+		Application.HasUserAuthorization(UserAuthorization.Microphone);
+		yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+
+		if (Permission.HasUserAuthorizedPermission(Permission.Microphone))
+		{
+			Permission.RequestUserPermission(Permission.Microphone);
+		}
+
+		yield return StartCoroutine(StartMicRoutine());
+	}
+
+	private IEnumerator StartMicRoutine()
+	{
+		 Device = Microphone.devices.FirstOrDefault();
+
+		_audioSource.clip = Microphone.Start(Device, true, 1, AudioSettings.outputSampleRate);
+		_audioSource.loop = true;
+
+		yield return new WaitUntil(() => Microphone.GetPosition(Device) > 0);
+
+		Debug.Log($"Started recording: {Device} {AudioSettings.outputSampleRate}");
+		_audioSource.Play();
+	}
+
+	private void StopMic()
+	{
+		Value = 0f;
+		Microphone.End(Device);
+		_audioSource.clip = null;
+	}
+
+	private void Update()
+	{
+		if (_audioSource.clip == null) return;
+		var pos = Microphone.GetPosition(Device) - _sampleValues.Length + 1;
+
+		// Brute force, but instead of managing wrapped buffers (due to the looping). Just skip updating for the couple
+		// of invalid frames. This is not pretty, but hardly noticeable for users.
+		if (pos < 0) return;
+
+		Value = 0;
+		_audioSource.clip.GetData(_sampleValues, pos);
+
+		for (var i = 0; i < _sampleValues.Length - 1; i++)
+		{
+			Value = Mathf.Max(Value, _sampleValues[i]);
+		}
+
+#if UNITY_EDITOR
+		Value = Input.GetKey(KeyCode.B) ? 0.5f : Value;
+#endif
+		Peak = Mathf.Max(Peak, Value);
+
+		if (_peakText != null) _peakText.text = $"Peak: {Peak:0.000}";
+		if (_positionText != null) _positionText.text = $"Position: {Microphone.GetPosition(Device)}";
+		if (_maxText != null) _maxText.text = $"Peak: {Value:0.000}";
+	}
+}
--- Assets/Scripts/SoapBubbleSpawner.cs
+++ Assets/Scripts/SoapBubbleSpawner.cs
@@ -1,31 +1,36 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
 public class SoapBubbleSpawner : MonoBehaviour
 {
-	[SerializeField] private MicrophoneStream _microphone;
+	[SerializeField] private ARSessionOrigin _origin;
+	[SerializeField] private Transform _arRoot;
+
+	[SerializeField] private MicrophoneInput _microphone;
 	[SerializeField] private SoapBubble _bubble;
 	[SerializeField] private Transform _bubbleSpawnHolder;
 
 	[SerializeField] private float _spawnThreshold = 0.4f;
 	[SerializeField] private float _releaseForce = 0.1f;
-	[SerializeField] private Vector3 _releaseDir = new Vector3(0,1f,0.3f);
+	[SerializeField] private Vector3 _releaseDir = new Vector3(0, 1f, 0.3f);
 	[SerializeField] private float _growSpeed = 0.01f;
 	[SerializeField] private float _maxSize = 0.1f;
+	[SerializeField] private float _maxTime = 2f;
+	[SerializeField] private float _intervalTime = 0.2f;
 
 	private SoapBubble _bubbleInProgress;
 	private Coroutine _spawnRoutine;
 
 	private void Reset()
 	{
-		_microphone = GetComponent<MicrophoneStream>();
+		_microphone = GetComponent<MicrophoneInput>();
 	}
 
 	private void OnEnable()
 	{
 		_spawnRoutine = StartCoroutine(SpawnRoutine());
+		_origin.MakeContentAppearAt(_arRoot, Vector3.zero);
 	}
 
 	private void OnDisable()
@@ -37,22 +42,31 @@ public class SoapBubbleSpawner : MonoBehaviour
 	{
 		while (enabled)
 		{
-			while (_microphone.RunningAverage < _spawnThreshold) yield return null;
+			while (_microphone.Value < _spawnThreshold) yield return null;
 
 			_bubbleInProgress = Instantiate(_bubble, _bubbleSpawnHolder, false);
-			var size = 0.01f;
-			while (_microphone.RunningAverage > _spawnThreshold && size < _maxSize)
-			{
-				size += _microphone.RunningAverage * _growSpeed;
 
-				_bubbleInProgress.SetSize(size);
-				yield return null;
+			{
+				var size = 0.01f;
+				var startTime = Time.realtimeSinceStartup;
+
+				while (_microphone.Value > _spawnThreshold && size < _maxSize &&
+				       Time.realtimeSinceStartup - (Input.anyKey ? _maxTime / 2f : _maxTime) < startTime)
+				{
+					size += _microphone.Value * _growSpeed;
+
+					_bubbleInProgress.SetSize(size);
+					yield return null;
+				}
 			}
 
-			_bubbleInProgress.Release((_bubbleSpawnHolder.TransformDirection(_releaseDir).normalized)* _releaseForce);
+			_bubbleInProgress.Release((_bubbleSpawnHolder.TransformDirection(_releaseDir).normalized) * _releaseForce, _origin.trackablesParent);
 			_bubbleInProgress = null;
 
-			while (_microphone.RunningAverage > _spawnThreshold) yield return null;
+			{
+				var startTime = Time.realtimeSinceStartup;
+				while (_microphone.Value > _spawnThreshold && Time.realtimeSinceStartup - _intervalTime < startTime) yield return null;
+			}
 		}
 	}
 }
